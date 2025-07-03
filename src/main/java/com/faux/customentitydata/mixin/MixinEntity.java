dea053fcf28435a735c8464d10fc4a61521d15a5
Jared <jaredlll08@gmail.com>
2025-07-03 14:39:46 -0600

    Port to 1.21.6

--- src/main/java/com/faux/customentitydata/mixin/MixinEntity.java
+++ src/main/java/com/faux/customentitydata/mixin/MixinEntity.java
@@ -2,16 +2,15 @@ package com.faux.customentitydata.mixin;
 
 import com.faux.customentitydata.api.ICustomDataHolder;
 import com.faux.customentitydata.api.PersistentEntityDataConstants;
-import net.minecraft.core.Registry;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.nbt.Tag;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public abstract class MixinEntity implements ICustomDataHolder {
@@ -32,14 +31,14 @@ public abstract class MixinEntity implements ICustomDataHolder {
         faux$persistentData = tag;
     }
 
-    @Inject(method = "saveWithoutId", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;addAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V", shift = At.Shift.BEFORE))
-    public void saveWithoutId(CompoundTag compoundTag, CallbackInfoReturnable<CompoundTag> cir) {
-        compoundTag.put(PersistentEntityDataConstants.CUSTOM_NBT_KEY, faux$getCustomData());
+    @Inject(method = "saveWithoutId", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;addAdditionalSaveData(Lnet/minecraft/world/level/storage/ValueOutput;)V", shift = At.Shift.BEFORE))
+    public void saveWithoutId(ValueOutput valueOutput, CallbackInfo ci) {
+        valueOutput.store(PersistentEntityDataConstants.CUSTOM_NBT_KEY, CompoundTag.CODEC, faux$getCustomData());
     }
 
-    @Inject(method = "load", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;readAdditionalSaveData(Lnet/minecraft/nbt/CompoundTag;)V", shift = At.Shift.BEFORE))
-    public void load(CompoundTag compoundTag, CallbackInfo ci) {
-        compoundTag.getCompound(PersistentEntityDataConstants.CUSTOM_NBT_KEY).ifPresent(this::faux$setCustomData);
+    @Inject(method = "load", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/entity/Entity;readAdditionalSaveData(Lnet/minecraft/world/level/storage/ValueInput;)V", shift = At.Shift.BEFORE))
+    public void load(ValueInput valueInput, CallbackInfo ci) {
+        valueInput.read(PersistentEntityDataConstants.CUSTOM_NBT_KEY, CompoundTag.CODEC).ifPresent(this::faux$setCustomData);
     }
 
 }
--- src/main/java/com/faux/customentitydata/mixin/MixinPlayerList.java
+++ src/main/java/com/faux/customentitydata/mixin/MixinPlayerList.java
@@ -4,13 +4,17 @@ import com.faux.customentitydata.api.playersaves.IPlayerLoadListener;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.players.PlayerList;
+import net.minecraft.util.ProblemReporter;
 import net.minecraft.world.level.storage.PlayerDataStorage;
+import net.minecraft.world.level.storage.ValueInput;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Optional;
 
 @Mixin(PlayerList.class)
 public class MixinPlayerList {
@@ -20,7 +24,7 @@ public class MixinPlayerList {
     private PlayerDataStorage playerIo;
 
     @Inject(method = "load", at = @At("RETURN"))
-    private void onPlayerLoad(ServerPlayer player, CallbackInfoReturnable<CompoundTag> info) {
+    private void onPlayerLoad(ServerPlayer player, ProblemReporter problemReporter, CallbackInfoReturnable<Optional<ValueInput>> cir) {
 
         if (this.playerIo instanceof AccessorPlayerDataStorage accessor) {
 
